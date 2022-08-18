88cef26f98b2151b2e8819e56a820c044484e2eb
cal135 <cal135@uclive.ac.nz>
2022-08-18 18:57:31 +1200

    First row of table highlighted when app opened

--- src/main/java/seng202/team3/gui/MainController.java
+++ src/main/java/seng202/team3/gui/MainController.java
@@ -162,6 +162,9 @@ public class MainController {
         chargerTable.getColumns().add(locationCol);
         chargerTable.getColumns().add(parksCol);
         chargerTable.getColumns().add(attractionCol);
+
+        chargerTable.requestFocus();
+        chargerTable.getSelectionModel().select(0);
     }
 
     /**
