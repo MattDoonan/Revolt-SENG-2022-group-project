560ab22317f788f7f167dfdde7415c9ec2623328
James Billows <jbi57@uclive.ac.nz>
2022-08-21 12:20:40 +1200

    Created vehicle class

---
+++ src/main/java/seng202/team3/data/entity/Vehicle.java
@@ -0,0 +1,25 @@
+package seng202.team3.data.entity;
+
+/**
+ * Stores information about vehicles
+ * 
+ * @author James Billows
+ * @version 1.0.0, Aug 21
+ */
+public class Vehicle {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
