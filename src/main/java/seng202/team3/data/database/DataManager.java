0bdfbf45dc8c1d68d0cc3c88863bd9e92fd9341e
Harrison Tyson <hty13@uclive.ac.nz>
2022-08-26 17:41:38 +1200

    Fixed checkstyle errors on branch to test pipeline

--- src/main/java/seng202/team3/data/database/DataManager.java
+++ src/main/java/seng202/team3/data/database/DataManager.java
@@ -1,5 +1,8 @@
 package seng202.team3.data.database;
 
+/**
+ * DataManager stub
+ */
 public interface DataManager {
 
 }
--- src/main/java/seng202/team3/logic/ChargerManager.java
+++ src/main/java/seng202/team3/logic/ChargerManager.java
@@ -1,5 +1,8 @@
 package seng202.team3.logic;
 
+/**
+ * Charger Manager stub
+ */
 public class ChargerManager {
 
 }
