eb6f25b4d460a7d71ec9af6a0647175ce55ff85c
James Billows <jbi57@uclive.ac.nz>
2022-09-12 23:33:05 +1200

    skeleton for journey GUI

---
+++ src/main/java/seng202/team3/gui/JourneyController.java
@@ -0,0 +1,45 @@
+package seng202.team3.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * Controller for journeySidebar.fxml
+ * 
+ * @author James Billows
+ * @version 1.0.0, Aug 22
+ */
+public class JourneyController {
+  
+  @FXML
+  private Button calculateRoute;
+
+  @FXML
+  private Button startMapFind;
+
+  @FXML
+  private Button destMapFind;
+
+  @FXML
+  private TextField startTextField;
+
+  @FXML
+  private TextField destTextField;
+
+  @FXML
+  private Button 
+
+  /**
+     * Initialize the window
+     *
+     * @param stage Top level container for this window
+     */
+    public void init(Stage stage) {
+      
+
+  }
+
+
+}
