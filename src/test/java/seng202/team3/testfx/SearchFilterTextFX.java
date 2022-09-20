63f0a851f49a30973ce299cd0a5307478217219b
aki81 <aki81@uclive.ac.nz>
2022-09-20 16:56:02 +1200

    Cucumber tests + removal of testFX

--- src/test/java/seng202/team3/testfx/SearchFilterTextFX.java
+++ src/test/java/seng202/team3/testfx/SearchFilterTextFX.java
@@ -2,19 +2,18 @@ package seng202.team3.testfx;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.ApplicationTest;
 import seng202.team3.data.entity.Charger;
-import seng202.team3.data.entity.Connector;
 import seng202.team3.gui.MainController;
 import seng202.team3.gui.MainWindow;
-import seng202.team3.logic.Calculations;
 
 /**
  * Code designed to test the searching and filtering of the Main Window
@@ -89,47 +88,6 @@ public class SearchFilterTextFX extends TestFXBase {
         for (Charger charger : chargers) {
             String lowerAddress = charger.getLocation().getAddress().toLowerCase();
             if (!lowerAddress.contains("christ")) {
-                isValid = false;
-            }
-        }
-        assertTrue(isValid);
-    }
-
-    @Test
-    public void filtersMultipleTicks() {
-        boolean isValid = true;
-        clickOn("#filters");
-        clickOn("#chargerTypes");
-        clickOn("#acButton");
-        clickOn("#chargingCost");
-        clickOn("#executeSearch");
-        ObservableList<Charger> chargers = controller.getManager().getCloseChargerData();
-        for (Charger charger : chargers) {
-            if (charger.getChargeCost()) {
-                boolean hasAC = false;
-                for (Connector connector : charger.getConnectors()) {
-                    if (connector.getPower().equalsIgnoreCase("AC")) {
-                        hasAC = true;
-                    }
-                    if (!hasAC) {
-                        isValid = false;
-                    }
-                }
-                isValid = false;
-            }
-        }
-        assertTrue(isValid);
-    }
-
-    @Test
-    public void distanceFilterWorks() {
-        boolean isValid = true;
-        clickOn("#filters");
-        clickOn("#distanceDisplay");
-        clickOn("#executeSearch");
-        for (Charger charger : controller.getManager().getCloseChargerData()) {
-            if (Calculations.calculateDistance(charger.getLocation(),
-                    controller.getManager().getPosition()) > controller.getManager().getDistance()) {
                 isValid = false;
             }
         }
