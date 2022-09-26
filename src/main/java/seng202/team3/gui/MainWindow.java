3cc02d8a32d6a564953cf97f738a1045dd758534
Matthew <mdo79@uclive.ac.nz>
2022-09-27 00:41:58 +1300

    Auto screen size added

--- src/main/java/seng202/team3/gui/MainWindow.java
+++ src/main/java/seng202/team3/gui/MainWindow.java
@@ -5,6 +5,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.transform.Scale;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -34,7 +36,9 @@ public class MainWindow extends Application {
         MenuController baseController = baseLoader.getController();
         baseController.init(primaryStage);
         primaryStage.setTitle("Revolt App");
-        Scene scene = new Scene(root, 1500, 820);
+        double height = Screen.getPrimary().getBounds().getHeight()-80;
+        double width = Screen.getPrimary().getBounds().getWidth();
+        Scene scene = new Scene(root, width, height);
         baseController.initHome();
         primaryStage.setScene(scene);
         primaryStage.show();
