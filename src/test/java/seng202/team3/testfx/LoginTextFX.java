b604a68eb22b6a2a5314e42b92f7d8b7512a058f
Matthew <mdo79@uclive.ac.nz>
2022-09-30 11:48:35 +1300

    Done LoginTextFX Don't know how to do on login success test yet

---
+++ src/test/java/seng202/team3/testfx/LoginTextFX.java
@@ -0,0 +1,88 @@
+package seng202.team3.testfx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import net.bytebuddy.implementation.ExceptionMethod;
+import org.apache.commons.collections.functors.NullIsExceptionPredicate;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobotException;
+import org.testfx.framework.junit5.ApplicationTest;
+import seng202.team3.data.database.SqlInterpreter;
+import seng202.team3.data.entity.PermissionLevel;
+import seng202.team3.data.entity.User;
+import seng202.team3.gui.*;
+
+import java.io.IOException;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class LoginTextFX extends TestFxBase{
+
+
+    private LoginSignupController controller;
+
+    private User u;
+
+    @Override
+    public void setUp() throws Exception {
+        ApplicationTest.launch(MainWindow.class);
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent page = loader.load();
+        initState(loader, stage);
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initState(FXMLLoader loader, Stage stage) {
+        controller = loader.getController();
+        controller.init(new MenuController());
+    }
+
+    @BeforeEach
+    public void setup() throws IOException {
+        u = new User("test@gmail.com", "Tester", PermissionLevel.USER);
+        SqlInterpreter.getInstance().writeUser(u, "1234");
+    }
+
+    @AfterEach
+    public void reset() throws IOException {
+        SqlInterpreter.getInstance().deleteData("user", u.getUserid());
+    }
+
+
+    @Test
+    public void testFailedLogin() {
+        clickOn("#loginBtn");
+        verifyThat("#invalidLogin", Node::isVisible);
+    }
+
+    @Test
+    public void testInvalidUsername() {
+        clickOn("#loginEmailField");
+        write("This should fail");
+        clickOn("#loginBtn");
+        verifyThat("#invalidLogin", Node::isVisible);
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        clickOn("#loginEmailField");
+        write("admin");
+        clickOn("#loginPasswordField");
+        write("fake");
+        clickOn("#loginBtn");
+        verifyThat("#invalidLogin", Node::isVisible);
+    }
+}
