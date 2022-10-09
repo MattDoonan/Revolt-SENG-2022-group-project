f424a043abdf1894155ec57c5c7adba6aa43beac
James <jbi57@uclive.ac.nz>
2022-10-09 21:05:25 +1300

    Reverting to the state as at e2a39dc1

--- src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
+++ src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
@@ -1,6 +1,5 @@
 package seng202.team3.unittest.logic;
 
-import javafx.collections.ObservableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
@@ -8,8 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seng202.team3.data.database.SqlInterpreter;
 import seng202.team3.data.entity.Charger;
-import seng202.team3.data.entity.Connector;
-import seng202.team3.data.entity.Coordinate;
 import seng202.team3.data.entity.User;
 import seng202.team3.logic.ChargerHandler;
 import seng202.team3.logic.ChargerManager;
@@ -17,8 +14,6 @@ import seng202.team3.logic.GarageManager;
 import seng202.team3.logic.JourneyManager;
 
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for {@link JourneyManager journeyManager} logic class
@@ -33,26 +28,13 @@ public class JourneyManagerTest {
     private JourneyManager manager;
     static SqlInterpreter db;
     static User testUser;
-    private Charger charger1;
-    private Charger charger2;
-    private ObservableList<Charger> chargerList;
 
     /**
      * null
      */
     @BeforeEach
     public void setUp() {
-        Connector dummyConnector = new Connector("ChardaMo", "AC", "Available", "123", 3);
-        ArrayList<Connector> connectorList = new ArrayList<>(1);
-        connectorList.add(dummyConnector);
-        Coordinate coord = new Coordinate(4.5, 5.7, -36.85918, 174.76602);
-        charger1 = new Charger(connectorList, "Test1", coord, 1, 0.3,
-                "Meridian", "2020/1/1 00:00:00", true, true, true, true);
-        charger2 = new Charger(connectorList, "Test2", coord, 1, 0.3,
-                "Meridian", "2020/1/1 00:00:00", true, true, true, true);
 
-        chargerList.add(charger1);
-        chargerList.add(charger2);
     }
 
     /**
@@ -60,9 +42,7 @@ public class JourneyManagerTest {
      */
     @AfterEach
     public void tearDown() {
-        charger1 = null;
-        charger2 = null;
-        chargerList = null;
+
     }
 
     /**
@@ -74,30 +54,17 @@ public class JourneyManagerTest {
         //chargerHandler.makeAllChargers();
 
         //chargerHandler.setPosition(5765876);
-        //manager.setDesiredRange(50.0);
-        //manager.setCurrentCoordinate(new Coordinate());
+        //manager.setDesiredRange(45476);
 
         //list = new ChargerManager().getNearbyChargers(chargers,
                 //currentCoordinate, desiredRange);
     }
 
     /**
-     * Tests removeLastCharger() works as intended
-     * Utilizes boundary value of charger list size 1
+     * null
      */
     @Test
     public void testRemovingCharger() {
-
-        //manager.addCharger(chargerList.get(0));
-        //manager.addCharger(chargerList.get(1));
-        //manager.removeLastCharger(); // Removing with list size of 2
-        //assertFalse(manager.getChargers().contains(chargerList.get(1)));
-
-        //manager.removeLastCharger(); // Removing with list size of 1
-        //assertNull(manager.getChargers());
-
-        //manager.removeLastCharger(); // Removing with list size of 0
-        //assertNull(manager.getChargers());
 
     }
 
