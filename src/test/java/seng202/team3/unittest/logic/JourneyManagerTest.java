2093ee02a381f1c7b38043416c1a2fbf2223412e
James <jbi57@uclive.ac.nz>
2022-10-09 15:58:11 +1300

    Saving more small changes due to changing pc

--- src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
+++ src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
@@ -4,10 +4,23 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import seng202.team3.data.database.SqlInterpreter;
+import seng202.team3.data.entity.Charger;
 import seng202.team3.data.entity.User;
+import seng202.team3.logic.ChargerHandler;
+import seng202.team3.logic.ChargerManager;
+import seng202.team3.logic.GarageManager;
 import seng202.team3.logic.JourneyManager;
 
+import java.util.ArrayList;
+
+/**
+ * Unit tests for {@link JourneyManager journeyManager} logic class
+ *
+ * @author James Billows
+ * @version 1.0.0, Oct 22
+ */
 public class JourneyManagerTest {
 
     private static final Logger logManager = LogManager.getLogger();
@@ -16,6 +29,9 @@ public class JourneyManagerTest {
     static SqlInterpreter db;
     static User testUser;
 
+    /**
+     * null
+     */
     @BeforeEach
     public void setUp() {
 
@@ -26,6 +42,37 @@ public class JourneyManagerTest {
      */
     @AfterEach
     public void tearDown() {
+
+    }
+
+    /**
+     * null
+     */
+    @Test
+    public void testRangeChargers() {
+        //ChargerHandler chargerHandler = new ChargerHandler();
+        //chargerHandler.makeAllChargers();
+
+        //chargerHandler.setPosition(5765876);
+        //manager.setDesiredRange(45476);
+
+        //list = new ChargerManager().getNearbyChargers(chargers,
+                //currentCoordinate, desiredRange);
+    }
+
+    /**
+     * null
+     */
+    @Test
+    public void testRemovingCharger() {
+
+    }
+
+    /**
+     * null
+     */
+    @Test
+    public void testDistanceBetweenChargers() {
 
     }
 
