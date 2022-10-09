c6bd2fd8aae328f29e51b9a8b548a9822bf43c2f
James <jbi57@uclive.ac.nz>
2022-10-09 15:00:48 +1300

    Saving small changes due to changing pc

--- src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
+++ src/test/java/seng202/team3/unittest/logic/JourneyManagerTest.java
@@ -1,12 +1,32 @@
 package seng202.team3.unittest.logic;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import seng202.team3.data.database.SqlInterpreter;
+import seng202.team3.data.entity.User;
+import seng202.team3.logic.JourneyManager;
 
 public class JourneyManagerTest {
 
+    private static final Logger logManager = LogManager.getLogger();
+
+    private JourneyManager manager;
+    static SqlInterpreter db;
+    static User testUser;
 
     @BeforeEach
     public void setUp() {
 
     }
+
+    /**
+     * AfterEach, sets everything to null.
+     */
+    @AfterEach
+    public void tearDown() {
+
+    }
+
 }
